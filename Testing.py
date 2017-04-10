bc1d1bcb6500c8e32bccae681724532e0f653627
Dimitrisl <dlolis88@gmail.com>
2017-04-11 01:07:13 +0300

    some stuff

--- Testing.py
+++ Testing.py
@@ -1,5 +1,4 @@
 import sqlite3
-import os
 from flask import Flask,g,request
 from flask.helpers import url_for
 from flask.templating import render_template
@@ -8,9 +7,14 @@ from flask import abort
 app = Flask(__name__)
 
 def connect_db():
-    return sqlite3.connect("test.db")
+    return sqlite3.connect("C:\Users\Dimitrisl\Desktop\Testing\\test.db")
 
 @app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html",x=None)
+
+@app.route('/database')
 def hello_world():
     db_connection = connect_db()
     cursor = db_connection.execute("select * from user;")
@@ -19,7 +23,11 @@ def hello_world():
     print records[0]["username"]
     return render_template("index.html",x=records)
 
-@app.route('/paparia')
+@app.route('/register')
+def noaccount():
+    return render_template("register.html")
+
+@app.route('/list')
 def skata():
     lista = ["apples","oranges"]
     #abort(404)
